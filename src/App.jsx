e425e1d57c8c1236fccd72954199b16b4177fced
Hugo Sandoval <warrior.elite.mode@gmail.com>
2020-08-01 21:46:27 -0500

    Platillo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+import Platillo from "./Components/Platillo";
+
+import platillo from "./data/platillo.json";
 
 function App() {
   return (
     <main>
-      
+      <Container>
+        <Row>
+          <Col xs="12" sm="6" md="4">
+            <Platillo data={platillo} />
+          </Col>
+        </Row>
+      </Container>
     </main>
   );
 }
